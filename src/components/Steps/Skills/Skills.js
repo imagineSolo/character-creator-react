b8317654b939da26d6caade3de2046458b3685d8
Tomek <soltom6@gmail.com>
2020-02-10 22:16:29 +0100

    Working on Skills component

--- src/components/Steps/Skills/Skills.js
+++ src/components/Steps/Skills/Skills.js
@@ -1,64 +1,66 @@
-import React from "react";
+import React, { Component } from "react";
 import Apply from "../../Button/Apply/Apply";
 import Undo from "../../Button/Undo/Undo";
 import styles from "./Skills.module.scss";
 
-const skills = props => {
-  // const keysSkills = Object.keys(props.skills);
-  // const valuesSkills = Object.values(props.skills);
-  // console.log(keysSkills);
-  // console.log(valuesSkills);
+class Skills extends Component {
+  state = {
+    skills: {
+      arcana: false,
+      athletics: false,
+      crafting: false,
+      deception: false,
+      history: false,
+      intimidation: false,
+      investigation: false,
+      medicine: false,
+      nature: false,
+      perception: false,
+      performance: false,
+      persuasion: false,
+      religion: false,
+      stealth: false,
+      survival: false,
+      trickery: false
+    }
+  };
 
-  // const trueSkills = Array.from(props.skills);
-  // console.log(trueSkills);
-  // let filteredSkills = trueSkills.filter(Boolean);
-  // console.log(filteredSkills);
+  render() {
+    const skills = Object.entries(this.state.skills);
 
-  const skills = [
-    "Arcana",
-    "Athletics",
-    "Crafting",
-    "Deception",
-    "History",
-    "Intimidation",
-    "Medicine",
-    "Nature",
-    "Perception",
-    "Performance",
-    "Persuasion",
-    "Religion",
-    "Stealth",
-    "Survival",
-    "Trickery"
-  ];
+    const mappedSkills = skills.map(([key, value]) => {
+      const skillsSheet = Object.entries(this.props.skills);
+      console.log(skillsSheet);
 
-  const mappedSkills = skills.map(skill => {
+      return value === false ? (
+        <label key={key} className={styles.SkillsLabel}>
+          <input
+            type="checkbox"
+            value={key}
+            name="skill"
+            // disabled={value === true ? true : false}
+            readOnly={value === true ? true : false}
+            onChange={() => this.props.submit(key)}
+          />
+          {key}
+        </label>
+      ) : null;
+    });
     return (
-      <label key={skill} className={styles.SkillsLabel}>
-        <input
-          type="checkbox"
-          value={skill}
-          name="skill"
-          onChange={() => props.submit(skill.toLowerCase())}
-        />
-        {skill}
-      </label>
-    );
-  });
-  return (
-    <div className={styles.Content}>
-      <h3>Add additional skills:</h3>
-      <p>
-        Select three additional skills of your choice to shape up your
-        character.
-      </p>
-      <form className={styles.SkillsForm}>{mappedSkills}</form>
-      <div className={styles.Buttons}>
-        <Undo undo={props.undo} />
-        <Apply apply={props.submit} />
+      <div className={styles.Content}>
+        <h3>Add additional skills:</h3>
+        <p>
+          Select three additional skills of your choice to shape up your
+          character.
+        </p>
+        <form className={styles.SkillsForm}>{mappedSkills}</form>
+        <div className={styles.Buttons}>
+          <Undo undo={this.props.undo} />
+          <Apply apply={this.props.submit} />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default skills;
+export default Skills;

ba0d2187c52651be7f0d264e02301ff58356adf3
Tomek <soltom6@gmail.com>
2020-02-18 22:23:21 +0100

    Working on Random Traits

--- src/components/Steps/Traits/Traits.js
+++ src/components/Steps/Traits/Traits.js
@@ -2,40 +2,37 @@ import React, { Component } from "react";
 import Undo from "../../Button/Undo/Undo";
 import styles from "./Traits.module.scss";
 
-// const physical = [
-//   "Has a nervous twitch",
-//   "Smells funny",
-//   "Gesticules vividly",
-//   "Has an interesting eye color",
-//   "Dresses well"
-// ];
-
-// const mental = [
-//   "Is ambitious",
-//   "Is shy",
-//   "Is very religious",
-//   "Courageous",
-//   "Sceptical",
-//   "Has a positive attitude"
-// ];
-
-// const social = [
-//   "Laughs loud",
-//   "Likes to gossip",
-//   "Likes to meet new people",
-//   "Trusts only his companions",
-//   "Drinks a lot",
-//   "Loves baked potatoes"
-// ];
-
-// console.log(physical);
-// console.log(mental);
-// console.log(social);
-
 class Traits extends Component {
   state = {
     input: "",
-    traits: []
+    active: "",
+    traits: [],
+    random: {
+      physical: [
+        "Has a nervous twitch",
+        "Smells funny",
+        "Gesticules vividly",
+        "Has an interesting eye color",
+        "Dresses well"
+      ],
+      mental: [
+        "Is ambitious",
+        "Is shy",
+        "Is very religious",
+        "Courageous",
+        "Sceptical",
+        "Has a positive attitude"
+      ],
+      social: [
+        "Laughs loud",
+        "Likes to gossip",
+        "Likes to meet new people",
+        "Trusts only his companions",
+        "Drinks a lot",
+        "Loves baked potatoes"
+      ],
+      traits: []
+    }
   };
 
   handleChange = e => {
@@ -49,25 +46,59 @@ class Traits extends Component {
     const arr = this.state.traits;
     arr.push(this.state.input);
     this.setState({
+      active: "custom",
       traits: arr
     });
   };
 
+  handleRandomTrait = () => {
+    const physical = this.state.random.physical;
+    const rndmPhysical = Math.floor(
+      Math.random() * this.state.random.physical.length
+    );
+    const mental = this.state.random.mental;
+    const rndmMental = Math.floor(
+      Math.random() * this.state.random.mental.length
+    );
+    const social = this.state.random.social;
+    const rndmSocial = Math.floor(
+      Math.random() * this.state.random.social.length
+    );
+    const rndmTraits = this.state.random.traits;
+    rndmTraits.push(physical[rndmPhysical]);
+    rndmTraits.push(mental[rndmMental]);
+    rndmTraits.push(social[rndmSocial]);
+
+    this.setState({
+      ...this.state,
+      active: "random",
+      random: {
+        ...this.state.random,
+        traits: rndmTraits
+      }
+    });
+  };
+
   render() {
-    const traitList = Array.from(this.state.traits);
-    traitList.map((trait, index) => {
+    const customTraits = this.state.traits.map((trait, index) => {
       return (
         <li key={index} className={styles.Item}>
           <span>{trait}</span>
         </li>
       );
     });
-    console.log(traitList);
+
+    const randomTraits = this.state.random.traits.map((trait, index) => {
+      return (
+        <li key={index} className={styles.Item}>
+          <span>{trait}</span>
+        </li>
+      );
+    });
 
     return (
       <div className={styles.Content}>
         <h3 className={styles.Header}>Personality Traits</h3>
-        <ul>{traitList}</ul>
         <form onSubmit={this.handleSubmit}>
           <label>Add personality trait:</label>
           <input
@@ -79,8 +110,13 @@ class Traits extends Component {
             onChange={this.handleChange}
           />
           <input type="submit" value="Add" />
-          <input type="button" value="Random traits" />
+          <input
+            type="button"
+            value="Random traits"
+            onClick={this.handleRandomTrait}
+          />
         </form>
+        <ul>{this.state.active === "custom" ? customTraits : randomTraits}</ul>
         <Undo undo={this.props.undo} />
       </div>
     );

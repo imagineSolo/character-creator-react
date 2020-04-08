69e1c7d3330d9d39cf6a0678b23578ed0ec3edfb
Tomek <soltom6@gmail.com>
2020-04-08 21:45:05 +0200

    Added Middleware / redux-thunk to project

--- src/index.js
+++ src/index.js
@@ -4,17 +4,34 @@ import "./index.module.scss";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+import uiReducer from "./store/reducers/ui";
+import stepsReducer from "./store/reducers/steps";
+import dataReducer from "./store/reducers/data";
 import registerServiceWorker from "./registerServiceWorker";
-import reducer from "./store/reducer";
 
 const rootReducer = combineReducers({
-  ui: 
-  steps:
-  data: 
-})
+  ui: uiReducer,
+  steps: stepsReducer,
+  data: dataReducer,
+});
 
-const store = createStore(reducer);
+const middleware = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log("[Middleware] Dispatching", action);
+      const result = next(action);
+      console.log("[Middleware] next state", store.getState());
+      return result;
+    };
+  };
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(middleware, thunk)));
 
 const app = (
   <Provider store={store}>

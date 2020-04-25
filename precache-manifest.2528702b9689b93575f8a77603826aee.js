bfa8aaa072bb3cd3beb7c2a4b21ac1bdd0810980
Tomek <soltom6@gmail.com>
2020-04-25 22:08:25 +0200

    Updates

---
+++ precache-manifest.2528702b9689b93575f8a77603826aee.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e852ba73ae6b2d1d50c0d012e9aec56c",
+    "url": "/character-creator-react/index.html"
+  },
+  {
+    "revision": "3ebe996cc6b004028589",
+    "url": "/character-creator-react/static/css/main.688e6ca4.chunk.css"
+  },
+  {
+    "revision": "69357ec006fb7a9d331c",
+    "url": "/character-creator-react/static/js/2.8234e202.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/character-creator-react/static/js/2.8234e202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ebe996cc6b004028589",
+    "url": "/character-creator-react/static/js/main.6854a374.chunk.js"
+  },
+  {
+    "revision": "66b8e08ccf5305f641e6",
+    "url": "/character-creator-react/static/js/runtime-main.8e6933fb.js"
+  },
+  {
+    "revision": "1ce787ae13da7cac5994c6d331e5db13",
+    "url": "/character-creator-react/static/media/DF_01.1ce787ae.png"
+  },
+  {
+    "revision": "32badfd23974d529ba9e2867b3a567a7",
+    "url": "/character-creator-react/static/media/DF_02.32badfd2.png"
+  },
+  {
+    "revision": "cdde0e5bb50bd2aefe8663128e4e8993",
+    "url": "/character-creator-react/static/media/DF_03.cdde0e5b.png"
+  },
+  {
+    "revision": "d9965b42f6e3184d76d04a8a42d3065d",
+    "url": "/character-creator-react/static/media/DF_04.d9965b42.png"
+  },
+  {
+    "revision": "f11bb748157091201705e2c2568e2e2d",
+    "url": "/character-creator-react/static/media/DM_01.f11bb748.png"
+  },
+  {
+    "revision": "2143c43d44f706e62fd579d342423e5e",
+    "url": "/character-creator-react/static/media/DM_02.2143c43d.png"
+  },
+  {
+    "revision": "6361fd978b429879007798069f27db70",
+    "url": "/character-creator-react/static/media/DM_03.6361fd97.png"
+  },
+  {
+    "revision": "4267a23602aff86f51fed3a3a3fb558e",
+    "url": "/character-creator-react/static/media/DM_04.4267a236.png"
+  },
+  {
+    "revision": "d3361c3dbfb416e4e203e664e13ad91c",
+    "url": "/character-creator-react/static/media/EF_01.d3361c3d.png"
+  },
+  {
+    "revision": "b1f3773995512f63e667e55f34d4c3e1",
+    "url": "/character-creator-react/static/media/EF_02.b1f37739.png"
+  },
+  {
+    "revision": "a847a03430ff35edae00db9bb0411641",
+    "url": "/character-creator-react/static/media/EF_03.a847a034.png"
+  },
+  {
+    "revision": "54b3114f9240e9f58a8894ce2b1b15ab",
+    "url": "/character-creator-react/static/media/EF_04.54b3114f.png"
+  },
+  {
+    "revision": "829a3083b1184f1512ef42d1860cfcf5",
+    "url": "/character-creator-react/static/media/EM_01.829a3083.png"
+  },
+  {
+    "revision": "0469d759ec693beeee0b32925cbf9142",
+    "url": "/character-creator-react/static/media/EM_02.0469d759.png"
+  },
+  {
+    "revision": "de25e2c8622ec66dbfeff62103f53ec7",
+    "url": "/character-creator-react/static/media/EM_03.de25e2c8.png"
+  },
+  {
+    "revision": "6a40d53239fee9bf6942639429ebe0a3",
+    "url": "/character-creator-react/static/media/EM_04.6a40d532.png"
+  },
+  {
+    "revision": "22a933d2cdfd323bb75aab7de8919abf",
+    "url": "/character-creator-react/static/media/HF_01.22a933d2.png"
+  },
+  {
+    "revision": "4451174e4ad785ee89dda7655050c0a8",
+    "url": "/character-creator-react/static/media/HF_02.4451174e.png"
+  },
+  {
+    "revision": "7628835e36b92a0b063b92b2cbe9952a",
+    "url": "/character-creator-react/static/media/HF_03.7628835e.png"
+  },
+  {
+    "revision": "6a340e9d8e39b6925b34912d60b67767",
+    "url": "/character-creator-react/static/media/HF_04.6a340e9d.png"
+  },
+  {
+    "revision": "3600cd9b9cdc19c3088931f4bd3e7969",
+    "url": "/character-creator-react/static/media/HLF_01.3600cd9b.png"
+  },
+  {
+    "revision": "78d7e65c0890e19075208caaed07e35e",
+    "url": "/character-creator-react/static/media/HLF_02.78d7e65c.png"
+  },
+  {
+    "revision": "ddd8b58333600988ffa32b235bbec67f",
+    "url": "/character-creator-react/static/media/HLF_03.ddd8b583.png"
+  },
+  {
+    "revision": "1f3eb28bd42018528417c4e6b02fd9e0",
+    "url": "/character-creator-react/static/media/HLF_04.1f3eb28b.png"
+  },
+  {
+    "revision": "22f55c97431206a7c34b382272cb6681",
+    "url": "/character-creator-react/static/media/HLM_01.22f55c97.png"
+  },
+  {
+    "revision": "7624e42804dba5a0678c880c43901add",
+    "url": "/character-creator-react/static/media/HLM_02.7624e428.png"
+  },
+  {
+    "revision": "c8f2c0b2f6cfa42bc8acb7af615ab309",
+    "url": "/character-creator-react/static/media/HLM_03.c8f2c0b2.png"
+  },
+  {
+    "revision": "7f104419f320abdc89a6323c91c9f7f5",
+    "url": "/character-creator-react/static/media/HLM_04.7f104419.png"
+  },
+  {
+    "revision": "2143eb72d230a267d9097c2c74e3cb25",
+    "url": "/character-creator-react/static/media/HM_01.2143eb72.png"
+  },
+  {
+    "revision": "21f0ea4191d6e47c93ad211a00af3ebe",
+    "url": "/character-creator-react/static/media/HM_02.21f0ea41.png"
+  },
+  {
+    "revision": "6fbd4ffcd90de19a2b2a713140dfec1d",
+    "url": "/character-creator-react/static/media/HM_03.6fbd4ffc.png"
+  },
+  {
+    "revision": "a7d57d4afec0ea03c3d887830174ba96",
+    "url": "/character-creator-react/static/media/HM_04.a7d57d4a.png"
+  },
+  {
+    "revision": "a5c3e8377845c9408542a737d2a2d505",
+    "url": "/character-creator-react/static/media/Paper.a5c3e837.jpg"
+  },
+  {
+    "revision": "d816127e34febd483e887ded8d8836e0",
+    "url": "/character-creator-react/static/media/TF_01.d816127e.png"
+  },
+  {
+    "revision": "7dba14f4f1044da2be64195c18b180a0",
+    "url": "/character-creator-react/static/media/TF_02.7dba14f4.png"
+  },
+  {
+    "revision": "cd6d70309464a6bbd0119681d5d37f93",
+    "url": "/character-creator-react/static/media/TF_03.cd6d7030.png"
+  },
+  {
+    "revision": "b5a14599f7551dd3ae85d10204a27f57",
+    "url": "/character-creator-react/static/media/TF_04.b5a14599.png"
+  },
+  {
+    "revision": "71f94a1fad17b537f18f0691635019dc",
+    "url": "/character-creator-react/static/media/TM_01.71f94a1f.png"
+  },
+  {
+    "revision": "783630a98004c3af14bf94d2d993491d",
+    "url": "/character-creator-react/static/media/TM_02.783630a9.png"
+  },
+  {
+    "revision": "64c90311d932c14687e767ed6c625cf8",
+    "url": "/character-creator-react/static/media/TM_03.64c90311.png"
+  },
+  {
+    "revision": "c3e085358ead24ccbc58e55e5944bc5f",
+    "url": "/character-creator-react/static/media/TM_04.c3e08535.png"
+  },
+  {
+    "revision": "8e858bc02c115a4f01e3778fab57b086",
+    "url": "/character-creator-react/static/media/inkwell.8e858bc0.png"
+  }
+]);
